a8d21e4af0890a9c65e826b938e4b8cd97951a67
ADON15c <adonisclark@icloud.com>
2025-03-31 17:56:32 -0600

    Spawnpoint Triggers only trigger on grounded player

--- scenes/spawnpoint_trigger/spawnpoint_trigger.gd
+++ scenes/spawnpoint_trigger/spawnpoint_trigger.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var spawnpoint: Node2D
 var level: Level
+var contained_players: Array[CharacterBody2D]
 
 func _ready():
 	var parent: Node = get_parent()
@@ -14,7 +15,16 @@ func _ready():
 	
 	if level:	
 		body_entered.connect(_on_body_entered)
+		body_exited.connect(_on_body_exited)
+
+func _process(_delta: float) -> void:
+	for player in contained_players:
+		if player.is_on_floor():
+			level.spawn_point = spawnpoint.position
 
 func _on_body_entered(body):
 	if body == level.player:
-		level.spawn_point = spawnpoint.position
+		contained_players.append(body)
+
+func _on_body_exited(body):
+	contained_players = contained_players.filter(func(p): return p != body)
