c0876d6930eaa55e4ee0446c631d881e29d66ec1
ADON15c <adonisclark@icloud.com>
2025-04-05 18:04:59 -0600

    Pause Menu

---
+++ scenes/level_container/pause_menu.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+@onready var select = $Select
+
+func _ready() -> void:
+	select.button_clicked.connect(menu_select)
+	visible = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if !get_tree().paused:
+			get_tree().paused = true
+			visible = true
+		else:
+			get_tree().paused = false
+			visible = false
+
+
+func menu_select(id: String):
+	match id:
+		"unpause":
+			get_tree().paused = false
+			visible = false
+		"mainMenu":
+			get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+		"quitGame":
+			get_tree().quit()
