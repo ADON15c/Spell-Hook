bbf25e6346781ddeca407314849564b168e4ec09
ADON15c <adonisclark@icloud.com>
2025-04-08 00:44:50 -0600

    Finished Cutscenes

--- scenes/cutscene_choice/cutscene_choice.gd
+++ scenes/cutscene_choice/cutscene_choice.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var stable_button: Button = $HBoxContainer/Control3/StableButton
 
 var bad_cutscene: Cutscene = load("res://resources/cutscenes/game_end/game_end_bad.tres")
+var neutral_cutscene: Cutscene = load("res://resources/cutscenes/game_end/game_end_neutral.tres")
 var pray_cutscene: Cutscene = load("res://resources/cutscenes/game_end/game_end_good.tres")
 var stable_cutscene: Cutscene = load("res://resources/cutscenes/game_end/game_end_true.tres")
 
@@ -14,8 +15,14 @@ func _ready():
 	if collected_stabilizers < 6:
 		stable_button.disabled = true
 	
-	bad_button.button_down.connect(func(): SceneSwitcher.load_cutscene(bad_cutscene))
+	bad_button.button_down.connect(roll_the_dice)
 	pray_button.button_down.connect(func(): SceneSwitcher.load_cutscene(pray_cutscene))
 	stable_button.button_down.connect(func(): SceneSwitcher.load_cutscene(stable_cutscene))
 	
 	pray_button.grab_focus()
+
+func roll_the_dice():
+	if randf() > 0.3:
+		SceneSwitcher.load_cutscene(bad_cutscene)
+	else:
+		SceneSwitcher.load_cutscene(neutral_cutscene)
